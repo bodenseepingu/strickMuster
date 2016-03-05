58c78e44c71538b9d7f06342defba48e41875e88
bodenseepingu <bodenseepinguin@onlinehome.de>
2016-03-05 10:28:03 +0100

    added license

--- src/strickmuster/StrickMuster.java
+++ src/strickmuster/StrickMuster.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * free for use for everybody without any restrictions 
  */
 package strickmuster;
 
